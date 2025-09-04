f3f3c79e804c1ee0dadfc6aa51f7a3f4bd411c21
Radoslav Romanyszyn <radoslav.romanyszyn@gmail.com>
2025-09-04 17:25:13 +0200

    Refactor and update 03/scoring.py

--- 03/scoring.py
+++ 03/scoring.py
@@ -2,30 +2,52 @@
 
 import sys
 
+def add_score(split_line, scores):
+    team_name = split_line[0]
+    if team_name in scores:
+        scores[team_name] += int(split_line[2])
+    else:
+        scores[team_name] = int(split_line[2])
+
+def print_summary(title, scores):
+    print(' '.join(title))
+    
+    for team_name in sorted(scores):
+        print("  {}: {}".format(team_name, scores[team_name]))        
+        
+def write_to_csv(file_path, scores):
+    with open(file_path, "w") as f:
+        f.write("team,score\n")
+        for team_name in sorted(scores):
+            f.write("{},{}\n".format(team_name, scores[team_name]))
+            
+def print_podium(scores):
+    print("Medal podium")
+    
+    sorted_scores = sorted(scores.items(), key=lambda item: item[1], reverse=True)
+    for i in range(min(3, len(sorted_scores))):
+        team_name = sorted_scores[i][0]
+        print("  {}".format(team_name))
+
 def run_with_file(input_file):
     scores = {}
+    
     for line in input_file:
         line = line.strip()
         if (not line) or line.startswith('#'):
             continue
-        parts = line.split()
-        if parts[0] == "summary":
-            print(' '.join(parts[1:]))
-            for name in sorted(scores):
-                print("  {}: {}".format(name, scores[name]))
-        elif parts[0] == "add":
-            name = parts[1]
-            if name in scores:
-                scores[name] += int(parts[3])
-            else:
-                scores[name] = int(parts[3])
-        elif parts[0] == "csv":
-            with open(parts[1], "w") as f:
-                f.write("team,score\n")
-                for name in sorted(scores):
-                    f.write("{},{}\n".format(name, scores[name]))
+        split_line = line.split()
+        
+        if split_line[0] == "add":
+            add_score(split_line[1:], scores)
+        elif split_line[0] == "summary":
+            print_summary(split_line[1:], scores)
+        elif split_line[0] == "csv":
+            write_to_csv(split_line[1], scores)
+        elif split_line[0] == "podium":
+            print_podium(scores)
         else:
-            print("Unknown command ('{}')!".format(parts[0]))
+            print("Unknown command ('{}')!".format(split_line[0]))
 
 def main():
     if len(sys.argv) != 2:
