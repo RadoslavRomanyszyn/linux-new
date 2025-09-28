e0b8081e59f676791c730a0e931ef41cfddad652
Radoslav Romanyszyn <radoslav.romanyszyn@gmail.com>
2025-09-29 00:37:21 +0200

    Update 05/interactive_calc.sh (formatting)

--- 05/interactive_calc.sh
+++ 05/interactive_calc.sh
@@ -5,33 +5,33 @@ set -ueo pipefail
 input=/dev/stdin
 
 if [ $# -eq 1 ]; then
-	if [ "$1" = "-" ]; then
-		input=/dev/stdin
-	elif [ -f "$1" ]; then
-		input="$1"
-	else
-		echo ""$0": "$1": No such file" >&2
-		exit 1
-	fi
+    if [ "$1" = "-" ]; then
+        input=/dev/stdin
+    elif [ -f "$1" ]; then
+        input="$1"
+    else
+        echo """$0"": ""$1"": No such file" >&2
+        exit 1
+    fi
 fi
 
 result=0
 
 while read -r op num; do
-	case "$op" in
-		+)
-			result=$(( $result + $num ))
-			;;
-		-)
-			result=$(( $result - $num ))
-			;;
-		\*)
-			result=$(( $result * $num ))
-			;;
-		/)
-			result=$(( $result / $num ))
-			;;
-	esac
+    case "$op" in
+        +)
+            result=$(( result + num ))
+            ;;
+        -)
+            result=$(( result - num ))
+            ;;
+        \*)
+            result=$(( result * num ))
+            ;;
+        /)
+            result=$(( result / num ))
+            ;;
+    esac
 done <"$input"
 
 echo "= $result"
